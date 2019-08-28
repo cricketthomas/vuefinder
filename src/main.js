1a047b17c4e78380a6583988291fe61c759729d4
Arthur Thomas <acthomasiv@gmail.com>
2019-08-28 18:49:31 -0400

    pwa support
    adding pwa support with vue cli pwa plugin

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './firebase';
 import * as VueGoogleMaps from "vue2-google-maps";
 import VueChartkick from 'vue-chartkick'
 import Chart from 'chart.js'
+import './registerServiceWorker'
 
 
 Vue.use(VueChartkick, {
